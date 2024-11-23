10463be6b127e710eecb022e4bae4f1318b684cf
claireingham <claireingham75@gmail.com>
2024-11-23 06:16:00 +0100

    Update index.js
    Added API. Function does not work.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-function search(event){
-    event.preventDefault();
-    let searchInputElement=document.querySelector("#search-input)";
-    let city=searchInputElement.value;
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = `${temperature}°c`;
 }
 
-
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  let apiKey = "2ef76df8bbtdbob65420d8afef3dc0e3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={city}&key={apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -31,5 +40,5 @@ function formatDate(date) {
 let currentDate = new Date();
 let currentDateElement = document.querySelector("#current-date");
 currentDateElement.innerHTML = formatDate(currentDate);
-let searchForm=document.querySelector("#search-form");
+let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
