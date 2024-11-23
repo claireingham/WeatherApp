6a8dc9d853325f1da846e713d506ea25934680b3
claireingham <claireingham75@gmail.com>
2024-11-23 06:06:15 +0100

    Update index.js
    Added date function to index.js

---
+++ src/index.js
@@ -0,0 +1,35 @@
+function search(event){
+    event.preventDefault();
+    let searchInputElement=document.querySelector("#search-input)";
+    let city=searchInputElement.value;
+}
+
+
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+  if (minutes < 10) {
+    minutes = `0$(minutes)`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes},`;
+}
+let currentDate = new Date();
+let currentDateElement = document.querySelector("#current-date");
+currentDateElement.innerHTML = formatDate(currentDate);
+let searchForm=document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
