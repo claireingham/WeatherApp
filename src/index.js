4f5cb3ff8f36193ed1d95b933c2a5dd6be094ebb
claireingham <claireingham75@gmail.com>
2024-11-26 04:37:46 +0100

    Update JS - NOT WORKING
    Update JS to refresh weather (DOES NOT WORK)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
+}
+refreshWeather();
+function searchCity(city) {
+  let apiKey = "2ef76df8bbtdbob65420d8afef3dc0e3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+  cityElement.innerHTML = response.data.city;
+  searchCity(searchInput.value);
 }
 
 function formatDate(date) {
